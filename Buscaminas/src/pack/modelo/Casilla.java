1afe592d8dbb4f5afcb0246eed8448928e8fbf9b
Mikel <Mikel@Mikel>
2016-03-09 18:48:31 +0100

    aaa

--- Buscaminas/src/pack/modelo/Casilla.java
+++ Buscaminas/src/pack/modelo/Casilla.java
@@ -13,4 +13,29 @@ public class Casilla {
 		this.columna=pColumna;
 		this.listaVecinas=new ArrayList<Casilla>();
 	}
+
+	public int getFila() {
+		return fila;
+	}
+
+	public void setFila(int pFila) {
+		this.fila = pFila;
+	}
+
+	public int getColumna() {
+		return columna;
+	}
+
+	public void setColumna(int pColumna) {
+		this.columna = pColumna;
+	}
+
+	public ArrayList<Casilla> getListaVecinas() {
+		return listaVecinas;
+	}
+
+	public void setListaVecinas(ArrayList<Casilla> pListaVecinas) {
+		this.listaVecinas = pListaVecinas;
+	}
+	
 }
