5b42c3b527f74f5344b7429145567d051cc99050
Universidad <Universidad@10.106.26.198>
2016-03-14 14:44:17 +0100

    cambio del metodo construirTAblero a abstracto

--- BuscaminasV1_0/src/pack/modelo/TableroBuilder.java
+++ BuscaminasV1_0/src/pack/modelo/TableroBuilder.java
@@ -3,12 +3,10 @@ package pack.modelo;
 public abstract class TableroBuilder {
 	protected Tablero tablero;
 	public TableroBuilder(){}
-	public void construirTablero(){
-		tablero =new Tablero();
-	}
+	public abstract void construirTablero();
 	public abstract void ponerFila();
 	public abstract void ponerColumna();
-	public abstract int calcularNumMinas();
+	public abstract void calcularNumMinas();
 	public void ponerCasillas(){
 		tablero.rellenarMatrix();
 	}
